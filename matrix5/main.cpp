d6510d12adf351c8bb7cd04177ac304c528e8634
Dora-17 <91243865+Dora-17@users.noreply.github.com>
2021-09-28 09:36:17 +0300

    Add files via upload

--- matrix5/main.cpp
+++ matrix5/main.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+   int v[100][100],n,S1=0,S2=0;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> v[i][j];
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                 if(i+j>n-1){
+                    S1=S1+v[i][j];
+                  if(i+j<n-1) {
+                    S2=S2+v[i][j];
+
+                  }
+                 }
+                }
+            }
+        }
+        cout << S1 << S2 << endl;
+    }
     return 0;
 }
